ea87bdd1ed5f0b322a5fb5332c07abc11c5da9e9
Mark <emp.yangchunyu@gmail.com>
2017-05-28 11:45:54 +0800

    INITIAL

---
+++ src/main/java/cn/edu/tongji/a508/controller/UserController.java
@@ -0,0 +1,20 @@
+package cn.edu.tongji.a508.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mark on 5/27/17.
+ */
+@RestController
+public class UserController {
+
+    /* Maps to all HTTP actions by default (GET,POST,..)*/
+    @RequestMapping("/users")
+    public @ResponseBody
+    String getUsers() {
+        return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+                "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+    }
+}
---
+++ src/main/java/cn/edu/tongji/a508/security/AccountCredentials.java
@@ -0,0 +1,26 @@
+package cn.edu.tongji.a508.security;
+
+/**
+ * Created by mark on 5/27/17.
+ */
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
